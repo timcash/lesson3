891c9c208cdd123ef9bceb4a08bbbef45211cc21
Tim <timcash@gmail.com>
2021-12-09 10:33:08 -0800

    adds posting

--- pages/api/events.js
+++ pages/api/events.js
@@ -2,12 +2,24 @@ const { PSDB } = require('planetscale-node')
 const conn = new PSDB('main')
 
 export default async function handler(req, res) {
-    try {
-        const [rows] = await conn.query('select * from events')
-        res.status(200).json(rows)
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({err:err.ToString()})
+    if (req.method === 'GET') {
+        try {
+            const [rows] = await conn.query('select * from events')
+            res.status(200).json(rows)
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({ err: err.ToString() })
+        }
+    }
+    if(req.method === 'POST') {
+        try {
+            const { title, event } = req.body
+            const [rows] = await conn.query('INSERT INTO events (title, created_at) VALUES (?, ?)', [title, event])
+            res.status(200).json(rows)
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({ err: err.ToString() })
+        }
     }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import useSWR from 'swr'
+import { useState } from 'react'
 
 function fetcher(url) {
   return fetch(url).then((r) => r.json())
@@ -31,24 +32,51 @@ function EventsInDatabase() {
   )
 }
 
+function CreateEvent() {
+  const [title, setTitle] = useState('')
+
+  const [date, setDate] = useState('')
+  return (
+    <div>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        fetch('/api/events', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            title,
+
+            date
+          })
+        })
+      }
+      }>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" value={date} onChange={(e) => setDate(e.target.value)} />
+        <button type="submit">Create Event</button>
+      </form>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <div className={styles.page}>
       <Head>
-        <title>Database 2</title>
-        <meta name="description" content="Database lesson 2" />
+        <title>Database 3</title>
+        <meta name="description" content="Database lesson 3" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1>
-          Database lesson 2
+          Database lesson 3
         </h1>
         <EventsInDatabase></EventsInDatabase>
+        <CreateEvent></CreateEvent>
       </main>
-
-      <footer className={styles.footer}>
-      </footer>
     </div>
   )
 }
