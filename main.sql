31cc41d96c36b31a13f0850f4afc0542b0f7fe6e
patree <treeshnapati@gmail.com>
2021-12-10 12:50:33 -0800

    wip/list-new-event

---
+++ main.sql
@@ -0,0 +1,2 @@
+-- DELETE all rows where date is empty
+DELETE FROM events WHERE date IS NULL;
--- pages/api/events.js
+++ pages/api/events.js
@@ -8,17 +8,17 @@ export default async function handler(req, res) {
             res.status(200).json(rows)
         } catch (err) {
             console.log(err)
-            res.status(500).json({ err: err.ToString() })
+            res.status(500).json({ err: err.toString() })
         }
     }
     if(req.method === 'POST') {
         try {
-            const { title, event } = req.body
-            const [rows] = await conn.query('INSERT INTO events (title, created_at) VALUES (?, ?)', [title, event])
+            const { title, created_at } = req.body
+            const [rows] = await conn.query('INSERT INTO events (title, created_at) VALUES (?, ?)', [title, created_at])
             res.status(200).json(rows)
         } catch (err) {
             console.log(err)
-            res.status(500).json({ err: err.ToString() })
+            res.status(500).json({ err: err.toString() })
         }
     }
 }
